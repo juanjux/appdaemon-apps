c2ddfc94addca30a24c7f38842edcbba8c7e75dc
Juanjo Alvarez <gorthol@pm.me>
2019-12-26 12:34:34 +0100

    Added the gate_lights app and example config

---
+++ gate_lights.py
@@ -0,0 +1,23 @@
+from base_app import BaseApp
+
+from local_config import gate_lights as config
+
+
+class GateLights(BaseApp):
+    def initialize(self):
+        for name, data in config['gates'].items():
+            self.listen_state(self.check_door, name, data=data)
+
+    def check_door(self, entity, attribute, old, new, kwargs):
+        d = kwargs['data']
+        if new == old or new != d['open_status']:
+            return
+
+        for l in config['lights']:
+            self.turn_on(l)
+
+        self.run_in(self.lights_off_cb, config['lights_timeout'])
+
+    def lights_off_cb(self, kwargs):
+        for l in config['lights']:
+            self.turn_off(l)
